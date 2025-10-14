e2fdbf79961e8c021e4691bbbbd6dedda3326746
noapatch <noapatch@post.bgu.ac.il>
2025-10-14 16:57:58 +0300

    adding real connection to mongoDB with Beanie and motor libraries

---
+++ Backend/database.py
@@ -0,0 +1,22 @@
+# database.py
+
+import os
+import motor.motor_asyncio
+from beanie import init_beanie
+from pydantic import BaseModel
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/tasksdb")
+
+
+async def init_db():
+    """
+    connect to mongo and get the db to beanie
+    """
+
+    client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI, uuidRepresentation="standard")
+
+    db = client.get_default_database()
+
+    await init_beanie(database=db, document_models=[])
+
+    print("✅ MongoDB connected successfully!")
--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from database import init_db
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello from Dockerized FastAPI backend!"}
+async def root():
+    return {"message": "Hello from FastAPI with Mongo!"}
