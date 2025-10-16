16d3a385b8005ce0258ddd281491d31d2ff7261a
Noa-Patchornik <noap2580@gmail.com>
2025-10-16 22:28:49 +0300

    add core middleware

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import init_db
 from tasks_router import router as tasks_router
 from tasks_update_router import router as tasks_update_router
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # או ["*"] לפיתוח
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 async def on_startup():
